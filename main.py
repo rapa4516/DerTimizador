3e960f0e602408374f2c851b8f476ec8c3daba19
Raphael Espinheiro Carmelo <raphael.carmelo@pmsc.local>
2025-04-16 13:48:54 -0300

    Verificação dos serviços que não estão em pleno funcionamento, mais ainda faltando ajustes nesses serviços (não tive tempo). Ajustado a possível posição do botão EXECUTE.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import tkinter as tk
 import os
-from PIL import Image, ImageTk  # <- ESSENCIAL para trabalhar com imagens
+from PIL import Image, ImageTk
 from utils.util_services import execute_command
 def on_click():
     execute_command()
@@ -11,9 +11,9 @@ janela.title("Otimizador Black Desert (by ZXCASDQWEASDZXC)")
 janela.geometry("680x402")
 janela.resizable(False, False)
 
-caminho_fundo = os.path.join("icons", "bg_main.png")  # Substitua pelo nome do seu arquivo
+caminho_fundo = os.path.join("icons", "bg_main.png")
 imagem_fundo_original = Image.open(caminho_fundo)
-imagem_fundo_redimensionada = imagem_fundo_original.resize((680, 402))  # Mesmo tamanho da janela
+imagem_fundo_redimensionada = imagem_fundo_original.resize((680, 402))
 imagem_fundo = ImageTk.PhotoImage(imagem_fundo_redimensionada)
 
 fundo = tk.Label(janela, image=imagem_fundo, bg="#222229")
@@ -21,7 +21,6 @@ fundo.place(x=0, y=0, relwidth=1, relheight=1)
 
 caminho_imagem = os.path.join("icons", "button_execute.png")
 imagem_original = Image.open(caminho_imagem)
-#imagem_redimensionada = imagem_original.resize((100, 100))
 imagem_botao = ImageTk.PhotoImage(imagem_original)
 
 # Criando o botão com a imagem
@@ -31,9 +30,10 @@ botao = tk.Button(
     command=on_click,
     bg="#1e1e1e",
     bd=0,
-    activebackground="#1e1e1e"
+    activebackground="#1e1e1e"   
 )
+
 botao.image = imagem_botao  # previne coleta de lixo
-botao.pack(pady=30)
+botao.place(relx=1.0, rely=1.0, anchor="se", x=-25, y=-25)
 
 janela.mainloop()
